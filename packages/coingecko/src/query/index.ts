bcbda5dd280268927a0d2b81ce8284ce7ea9f647
Juan <juan@MacBook-Air-de-Juan.local>
2021-10-17 17:40:08 +0200

    supportedCurrenies

--- packages/coingecko/src/query/index.ts
+++ packages/coingecko/src/query/index.ts
@@ -1,7 +1,7 @@
 import { JSON } from "@web3api/wasm-as";
 
 import { COINGECKO_API_URL } from "./config";
-import { HTTP_Query, HTTP_ResponseType, Ping } from "./w3";
+import { HTTP_Query, HTTP_ResponseType, Ping} from "./w3"; 
 
 export function ping(): Ping {
   const response = HTTP_Query.get({
@@ -25,4 +25,23 @@ export function ping(): Ping {
     };
   }
   throw Error(response.statusText);
+}
+
+export function supportedCurrenies(): String{
+  const response = HTTP_Query.get({
+    url: COINGECKO_API_URL + "/simple/supported_vs_currencies",
+    request: {
+      headers: [],
+      urlParams: [],
+      body: "",
+      responseType: HTTP_ResponseType.TEXT,
+    },
+  });
+  if (!response || response.status !== 200 || !response.body) {
+    throw Error(response.statusText);
+  }
+
+  const json = <JSON.Obj>JSON.parse(response.body);
+  console.log(json)
+  return "hola"
 }
