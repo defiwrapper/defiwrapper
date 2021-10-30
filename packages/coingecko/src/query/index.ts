fe208452b274c4f74bbfa4d8aa331c1209e71fc7
Juan <juan@MacBook-Air-de-Juan.local>
2021-10-31 00:18:16 +0200

    CoinList created, map parse error

--- packages/coingecko/src/query/index.ts
+++ packages/coingecko/src/query/index.ts
@@ -1,7 +1,7 @@
 import { JSON } from "@web3api/wasm-as";
 
 import { COINGECKO_API_URL } from "./config";
-import { HTTP_Query, HTTP_ResponseType, Ping } from "./w3";
+import { HTTP_Query, HTTP_ResponseType, Ping, CoinsList } from "./w3";
 
 export function ping(): Ping {
   const response = HTTP_Query.get({
@@ -48,4 +48,39 @@ export function supportedVSCurrencies(): Array<string> {
   }
   const valueArr = jsonArray.valueOf();
   return valueArr.map<string>((value) => value.toString());
+}
+
+export function coinsList(): Array<CoinsList>{
+  const response = HTTP_Query.get({
+    url: COINGECKO_API_URL + "/coins/list",
+    request: {
+      headers: [],
+      urlParams: [],
+      body: "",
+      responseType: HTTP_ResponseType.TEXT,
+    },
+  });
+
+  if (!response || response.status !== 200 || !response.body) {
+    throw Error(response.statusText);
+  }
+
+  const jsonArray = <JSON.Arr>JSON.parse(response.body);
+
+  if (!jsonArray) {
+    throw Error(response.statusText);
+  }
+  const valueArr = jsonArray.valueOf();
+  return valueArr.map<CoinsList> ( (elem) => {
+    if(elem.isObj){
+      const coinObj = elem as JSON.Obj
+      return {
+        id: coinObj.getString('id').toString(),
+        symbol: coinObj.getString('symbol').toString(),
+        name: coinObj.getString('name').toString()
+
+      } as CoinsList
+    }
+    throw new Error(" Array elemeent is not an object")
+  })
 }
