5a6efde524b229d0009baed629f9306eeca8e717
Juan <juan@MacBook-Air-de-Juan.local>
2021-11-04 17:26:06 +0100

    coinsMarkets type

--- packages/coingecko/src/query/index.ts
+++ packages/coingecko/src/query/index.ts
@@ -1,7 +1,7 @@
 import { JSON } from "@web3api/wasm-as";
 
 import { COINGECKO_API_URL } from "./config";
-import { HTTP_Query, HTTP_ResponseType, Ping, CoinsList, Input_coinsList } from "./w3";
+import { HTTP_Query, HTTP_ResponseType, Ping, coinsMarkets, Input_coinsList } from "./w3";
 
 export function ping(): Ping {
   const response = HTTP_Query.get({
@@ -27,35 +27,12 @@ export function ping(): Ping {
   throw Error(response.statusText);
 }
 
-export function supportedVSCurrencies(): Array<string> {
+
+export function coinsMarkets(): Array<coinsMarkets> {
+
+
   const response = HTTP_Query.get({
-    url: COINGECKO_API_URL + "/simple/supported_vs_currencies",
-    request: {
-      headers: [],
-      urlParams: [],
-      body: "",
-      responseType: HTTP_ResponseType.TEXT,
-    },
-  });
-  if (!response || response.status !== 200 || !response.body) {
-    throw Error(response.statusText);
-  }
-
-  const jsonArray = <JSON.Arr>JSON.parse(response.body);
-
-  if (!jsonArray) {
-    throw Error(response.statusText);
-  }
-  const valueArr = jsonArray.valueOf();
-  return valueArr.map<string>((value) => value.toString());
-}
-
-export function coinsList(Input: Input_coinsList): Array<CoinsList> {
-  const inputs = Input.include_platform
-    ? `coins/list?include_platform=true`
-    : `coins/list?include_platform=false`;
-  const response = HTTP_Query.get({
-    url: COINGECKO_API_URL + inputs,
+    url: COINGECKO_API_URL + "/coins/markets?vs_currency=usd",
     request: {
       headers: [],
       urlParams: [],
@@ -74,14 +51,14 @@ export function coinsList(Input: Input_coinsList): Array<CoinsList> {
   }
   const valueArr = jsonArray.valueOf();
 
-  return valueArr.map<CoinsList>((elem) => {
+  return valueArr.map<coinsMarkets>((elem) => {
     if (elem.isObj) {
       const coinObj = elem as JSON.Obj;
       return {
         id: (coinObj.getString("id") as JSON.Str).toString(),
         symbol: (coinObj.getString("symbol") as JSON.Str).toString(),
         name: (coinObj.getString("name") as JSON.Str).toString(),
-      } as CoinsList;
+      } as coinsMarkets;
     }
     throw new Error(" Array elemeent is not an object");
   });
