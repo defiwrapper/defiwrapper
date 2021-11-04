58f15a70d9ac9e5c4089e1976d039951834c3432
Juan <juan@MacBook-Air-de-Juan.local>
2021-11-04 16:58:00 +0100

    input caps & urlParams fix

--- packages/coingecko/src/query/routes/coinsList.ts
+++ packages/coingecko/src/query/routes/coinsList.ts
@@ -1,31 +1,8 @@
 import { JSON } from "@web3api/wasm-as";
 
-import { COINGECKO_API_URL } from "./config";
-import { HTTP_Query, HTTP_ResponseType, Ping, CoinsList, Input_coinsList, HTTP_UrlParam } from "./w3";
+import { COINGECKO_API_URL } from "../config";
+import { HTTP_Query, HTTP_ResponseType, CoinsList, Input_coinsList, HTTP_UrlParam } from "./w3";
 
-export function ping(): Ping {
-  const response = HTTP_Query.get({
-    url: COINGECKO_API_URL + "/ping",
-    request: {
-      headers: [],
-      urlParams: [],
-      body: "",
-      responseType: HTTP_ResponseType.TEXT,
-    },
-  });
-  if (!response || response.status !== 200 || !response.body) {
-    throw Error(response.statusText);
-  }
-
-  const json = <JSON.Obj>JSON.parse(response.body);
-  const geckoSays = json.getString("gecko_says");
-  if (geckoSays) {
-    return {
-      gecko_says: geckoSays.valueOf(),
-    };
-  }
-  throw Error(response.statusText);
-}
 
 export function coinsList(input: Input_coinsList): Array<CoinsList> {
     const urlParams : Array<HTTP_UrlParam> = [
