27759145436796cd1b131cdce07aeba4cf61b3c7
Juan <juan@MacBook-Air-de-Juan.local>
2021-11-12 00:23:16 +0100

    schema of enpoint and first returns of the endpoint. Working on the number (float) type

--- packages/coingecko/src/query/routes/coinsMarkets.ts
+++ packages/coingecko/src/query/routes/coinsMarkets.ts
@@ -1,11 +1,11 @@
 import { JSON } from "@web3api/wasm-as";
 
 import { COINGECKO_API_URL } from "../config";
-import { HTTP_Query, HTTP_ResponseType, coinsMarkets, Input_coinsList } from "../w3";
+import { HTTP_Query, HTTP_ResponseType, CoinsMarkets } from "../w3";
 
 
 
-export function coinsMarkets(): Array<coinsMarkets> {
+export function coinsMarkets(): Array<CoinsMarkets> {
 
 
     const response = HTTP_Query.get({
@@ -28,14 +28,38 @@ export function coinsMarkets(): Array<coinsMarkets> {
     }
     const valueArr = jsonArray.valueOf();
   
-    return valueArr.map<coinsMarkets>((elem) => {
+    return valueArr.map<CoinsMarkets>((elem) => {
       if (elem.isObj) {
         const coinObj = elem as JSON.Obj;
         return {
           id: (coinObj.getString("id") as JSON.Str).toString(),
           symbol: (coinObj.getString("symbol") as JSON.Str).toString(),
           name: (coinObj.getString("name") as JSON.Str).toString(),
-        } as coinsMarkets;
+          image: (coinObj.getString("image") as JSON.Str).toString(),
+          //current_price: Float!
+          market_cap: parseInt((coinObj.getString("market_cap") as JSON.Str).toString()) as i32,
+          //market_cap_rank: Int!
+          //fully_diluted_valuation: Int!
+          //total_volume: Int!
+          //high_24h: Int!
+          //low_24h: Int!
+          //price_change_24h: Float!
+          //price_change_percentage_24h: Float!
+          //market_cap_change_24h: Float!
+          //market_cap_change_percentage_24h: Float!
+          //circulating_supply: Int!
+          //total_supply: Int!
+          //max_supply: Int!
+          //ath: Int!
+          //ath_change_percentage: Float!
+          ath_date: (coinObj.getString("ath_date") as JSON.Str).toString(),
+          //atl: coinObj.getString("ath_date") as JSON.Float,
+          //atl_change_percentage: Float!
+          atl_date: (coinObj.getString("atl_date") as JSON.Str).toString(),
+          //roi: Roi
+          last_updated: (coinObj.getString("last_updated") as JSON.Str).toString()
+
+        } as CoinsMarkets;
       }
       throw new Error(" Array elemeent is not an object");
     });
