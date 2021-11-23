730f8825596b577a56a70f7ac22ce8fad36acb32
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2021-11-23 14:20:38 +0100

    Coins/markets function (#73)

---
+++ packages/coingecko/src/query/routes/ping.ts
@@ -0,0 +1,28 @@
+import { JSON } from "@web3api/wasm-as";
+
+import { COINGECKO_API_URL } from "../config";
+import { HTTP_Query, HTTP_ResponseType, Ping } from "../w3";
+
+export function ping(): Ping {
+  const response = HTTP_Query.get({
+    url: COINGECKO_API_URL + "/ping",
+    request: {
+      headers: [],
+      urlParams: [],
+      body: "",
+      responseType: HTTP_ResponseType.TEXT,
+    },
+  });
+  if (!response || response.status !== 200 || !response.body) {
+    throw Error(response.statusText);
+  }
+
+  const json = <JSON.Obj>JSON.parse(response.body);
+  const geckoSays = json.getString("gecko_says");
+  if (geckoSays) {
+    return {
+      gecko_says: geckoSays.valueOf(),
+    };
+  }
+  throw Error("Invalid response body!");
+}
