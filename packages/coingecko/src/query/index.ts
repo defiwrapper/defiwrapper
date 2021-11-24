5be0bafbedf5fcbdd60f79cf91c8962fe0e01328
Juan <juan@MacBook-Air-de-Juan.local>
2021-11-24 11:36:32 +0100

    fixing spaces for merge

--- packages/coingecko/src/query/index.ts
+++ packages/coingecko/src/query/index.ts
@@ -1,7 +1,7 @@
 import { coinsMarkets } from "./routes/coinsMarkets";
 import { ping } from "./routes/ping";
-import { coinsList } from "./routes/coinsList";
 import { simplePrice } from "./routes/simplePrice";
 import { simpleTokenPrice } from "./routes/simpleTokenPrice";
+import { coinsList } from "./routes/coinsList";
 
 export { ping, coinsList, coinsMarkets, simplePrice, simpleTokenPrice };
