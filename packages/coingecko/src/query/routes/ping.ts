16ba626c15f1b56b1f6a3eb82cfd3d13b68ecdc8
Niraj Kamdar <nirajkamdar27@pm.me>
2021-12-01 00:23:03 +0530

    reproduce no error message bug

--- packages/coingecko/src/query/routes/ping.ts
+++ packages/coingecko/src/query/routes/ping.ts
@@ -4,25 +4,26 @@ import { COINGECKO_API_URL } from "../config";
 import { HTTP_Query, HTTP_ResponseType, Ping } from "../w3";
 
 export function ping(): Ping {
-  const response = HTTP_Query.get({
-    url: COINGECKO_API_URL + "/ping",
-    request: {
-      headers: [],
-      urlParams: [],
-      body: "",
-      responseType: HTTP_ResponseType.TEXT,
-    },
-  });
-  if (!response || response.status !== 200 || !response.body) {
-    throw Error(response.statusText);
-  }
-
-  const json = <JSON.Obj>JSON.parse(response.body);
-  const geckoSays = json.getString("gecko_says");
-  if (geckoSays) {
-    return {
-      gecko_says: geckoSays.valueOf(),
-    };
-  }
   throw Error("Invalid response body!");
+  // const response = HTTP_Query.get({
+  //   url: COINGECKO_API_URL + "/ping",
+  //   request: {
+  //     headers: [],
+  //     urlParams: [],
+  //     body: "",
+  //     responseType: HTTP_ResponseType.TEXT,
+  //   },
+  // });
+  // if (!response || response.status !== 200 || !response.body) {
+  //   throw Error(response.statusText);
+  // }
+
+  // const json = <JSON.Obj>JSON.parse(response.body);
+  // const geckoSays = json.getString("gecko_says");
+  // if (geckoSays) {
+  //   return {
+  //     gecko_says: geckoSays.valueOf(),
+  //   };
+  // }
+  // throw Error("Invalid response body!");
 }
