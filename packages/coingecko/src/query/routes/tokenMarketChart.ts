a0efb600d2508e21bf406c38d39ea632ae567c53
James C <webdev410219@gmail.com>
2021-12-02 21:34:19 +0330

    chore(coingecko): refactor tokenMarketChart code and use newly implemented utils

--- packages/coingecko/src/query/routes/tokenMarketChart.ts
+++ packages/coingecko/src/query/routes/tokenMarketChart.ts
@@ -1,14 +1,15 @@
 import { JSON } from "@web3api/assemblyscript-json";
-import { BigInt } from "as-bigint";
 import { COINGECKO_API_URL } from "../config";
+import {
+  normalizeTimestampMarketCapPairArray,
+  normalizeTimestampPricePairArray,
+  normalizeTimestampVolumePairArray,
+} from "../utils";
 import {
   HTTP_Query,
   HTTP_ResponseType,
   HTTP_UrlParam,
   Input_tokenMarketChart,
-  TimestampMarketCapPair,
-  TimestampPricePair,
-  TimestampVolumePair,
   TokenMarketChart,
 } from "../w3";
 
@@ -48,45 +49,21 @@ export function tokenMarketChart(input: Input_tokenMarketChart): TokenMarketChar
     throw new Error("error in response: prices should be an array");
   }
 
-  const prices = pricesArray._arr.map<TimestampPricePair>((item) => {
-    const priceItem = item as JSON.Arr;
-    return {
-      timestamp: BigInt.fromString(priceItem._arr[0].toString()),
-      price: priceItem._arr[1].toString(),
-    };
-  });
-
   // market_capstotalVolumeItem._arr[0] as JSON.Integer
   const marketCapsArray = obj.getArr("market_caps");
   if (marketCapsArray === null) {
     throw new Error("error in response: market_caps should be an array");
   }
 
-  const marketCaps = marketCapsArray._arr.map<TimestampMarketCapPair>((item) => {
-    const marketCapItem = item as JSON.Arr;
-    return {
-      timestamp: BigInt.fromString(marketCapItem._arr[0].toString()),
-      market_cap: marketCapItem._arr[1].toString(),
-    };
-  });
-
   // total_volumes
   const totalVolumesArray = obj.getArr("total_volumes");
   if (totalVolumesArray === null) {
-    throw new Error("error in response: total_volumes should not be an array");
+    throw new Error("error in response: total_volumes should be an array");
   }
 
-  const totalVolumes = totalVolumesArray._arr.map<TimestampVolumePair>((item) => {
-    const totalVolumeItem = item as JSON.Arr;
-    return {
-      timestamp: BigInt.fromString(totalVolumeItem._arr[0].toString()),
-      volume: totalVolumeItem._arr[1].toString(),
-    };
-  });
-
   return {
-    prices,
-    market_caps: marketCaps,
-    total_volumes: totalVolumes,
+    prices: normalizeTimestampPricePairArray(pricesArray),
+    market_caps: normalizeTimestampMarketCapPairArray(marketCapsArray),
+    total_volumes: normalizeTimestampVolumePairArray(totalVolumesArray),
   };
 }
