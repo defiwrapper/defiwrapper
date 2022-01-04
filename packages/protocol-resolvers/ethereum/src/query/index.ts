136f5085efa3ab14bf5e742cf2e7a18a31a18889
Niraj Kamdar <nirajkamdar27@pm.me>
2022-01-04 17:37:52 +0530

    feat: add forkedFrom in protocol

--- packages/protocol-resolvers/ethereum/src/query/index.ts
+++ packages/protocol-resolvers/ethereum/src/query/index.ts
@@ -3,73 +3,116 @@ import { Input_getProtocol, ProtocolResolver_Protocol } from "./w3";
 export function getProtocol(input: Input_getProtocol): ProtocolResolver_Protocol {
   if (input.token.name.startsWith("Curve.fi ") && input.token.name.endsWith(" Gauge Deposit")) {
     return {
-      name: "Curve gauge",
+      organization: "Curve.fi",
+      name: "Curve.fi gauge",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("Curve.fi ")) {
     return {
-      name: "Curve",
+      organization: "Curve.fi",
+      name: "Curve.fi pool",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("yearn ") || input.token.name.startsWith("iearn ")) {
     return {
-      name: "Yearn",
+      organization: "Yearn.finance",
+      name: "Yearn Vault",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.name.endsWith(" yVault") || input.token.name.startsWith("yExperimental")) {
     return {
-      name: "Yearn",
+      organization: "Yearn.finance",
+      name: "Yearn Vault",
       version: "2",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("Aave Interest bearing ")) {
     return {
-      name: "Aave",
+      organization: "Aave",
+      name: "Aave lending-borrowing",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("Aave interest bearing ")) {
     return {
-      name: "Aave",
+      organization: "Aave",
+      name: "Aave lending-borrowing",
       version: "2",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("Aave AMM Market ")) {
     return {
-      name: "Aave AMM",
+      organization: "Aave",
+      name: "Aave AMM pool",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.symbol == "UNI-V2") {
     return {
+      organization: "Uniswap",
       name: "Uniswap",
       version: "2",
+      forkedFrom: null,
     };
   } else if (input.token.name == "SushiSwap LP Token") {
     return {
+      organization: "Sushi",
       name: "Sushiswap",
       version: "1",
+      forkedFrom: {
+        organization: "Uniswap",
+        name: "Uniswap",
+        version: "2",
+        forkedFrom: null,
+      },
     };
   } else if (input.token.name == "LinkSwap LP Token") {
     return {
+      organization: "Linkswap",
       name: "Linkswap",
       version: "1",
+      forkedFrom: {
+        organization: "Uniswap",
+        name: "Uniswap",
+        version: "2",
+        forkedFrom: null,
+      },
     };
   } else if (input.token.name.startsWith("Compound ") && input.token.symbol.startsWith("c")) {
     return {
+      organization: "Compound",
       name: "Compound",
       version: "1",
+      forkedFrom: null,
     };
   } else if (input.token.name.startsWith("Cream ") && input.token.symbol.startsWith("cr")) {
     return {
+      organization: "Cream",
       name: "Cream",
       version: "1",
+      forkedFrom: {
+        organization: "Compound",
+        name: "Compound",
+        version: "1",
+        forkedFrom: null,
+      },
     };
   } else if (input.token.name == "SushiBar") {
     return {
+      organization: "Sushi",
       name: "Sushibar",
       version: "1",
+      forkedFrom: null,
     };
   } else {
     return {
+      organization: "Unknown",
       name: "Unknown",
-      version: "0",
+      version: null,
+      forkedFrom: null,
     };
   }
 }
