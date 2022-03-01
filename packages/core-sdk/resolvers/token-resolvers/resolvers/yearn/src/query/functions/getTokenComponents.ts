d50d8430dc66ceafe409e89697d557cb2229e89b
krisbitney <kristoferbitney@hotmail.com>
2022-03-01 12:11:43 -0300

    added draft Yearn token resolver protocol validator (untested)

--- packages/core-sdk/resolvers/token-resolvers/resolvers/yearn/src/query/functions/getTokenComponents.ts
+++ packages/core-sdk/resolvers/token-resolvers/resolvers/yearn/src/query/functions/getTokenComponents.ts
@@ -1,18 +1,15 @@
 import { BigInt } from "@web3api/wasm-as";
-import { Big } from "as-big/Big";
+import { Big } from "as-big";
 
-import { CURVE_ADDRESS_PROVIDER_ADDRESS } from "../constants";
-import { parseStringArray } from "../utils/parseArray";
 import {
   env,
   Ethereum_Query,
   Input_getTokenComponents,
-  Interface_Token,
   Interface_TokenComponent,
   QueryEnv,
+  Token_Query,
+  Token_TokenType,
 } from "../w3";
-import { Token_Query } from "../w3/imported/Token_Query";
-import { Token_TokenType } from "../w3/imported/Token_TokenType";
 
 export function getTokenComponents(input: Input_getTokenComponents): Interface_TokenComponent {
   if (env == null) throw new Error("env is not set");
@@ -27,76 +24,46 @@ export function getTokenComponents(input: Input_getTokenComponents): Interface_T
     throw new Error(`Token ${input.tokenAddress} is not a valid ERC20 token`);
   }
 
-  const registeryAddressResult = Ethereum_Query.callContractView({
-    address: CURVE_ADDRESS_PROVIDER_ADDRESS,
-    method: "function get_registry() view returns (address)",
-    args: null,
+  // get underlying token
+  const underlyingTokenAddressRes = Ethereum_Query.callContractView({
+    address: token.address,
+    method: "function want() external view returns (address)",
+    args: [],
     connection: connection,
-  }).unwrap();
-  const poolAddress = Ethereum_Query.callContractView({
-    address: registeryAddressResult,
-    method: "function get_pool_from_lp_token(address) view returns (address)",
-    args: [token.address],
-    connection: connection,
-  }).unwrap();
-  const totalCoinsResult = Ethereum_Query.callContractView({
-    address: registeryAddressResult,
-    method: "function get_n_coins(address) view returns (uint256)",
-    args: [poolAddress],
-    connection: connection,
-  }).unwrap();
-  const totalCoins: i32 = I32.parseInt(totalCoinsResult);
-
-  const coinsResult = Ethereum_Query.callContractView({
-    address: registeryAddressResult,
-    method: "function get_coins(address) view returns (address[8])",
-    args: [poolAddress],
-    connection: connection,
-  }).unwrap();
-  const coins: Array<string> = parseStringArray(coinsResult);
-
-  const balancesResult = Ethereum_Query.callContractView({
-    address: registeryAddressResult,
-    method: "function get_balances(address) view returns (uint256[8])",
-    args: [poolAddress],
-    connection: connection,
-  }).unwrap();
-  const balances: Array<string> = parseStringArray(balancesResult);
-
-  const components = new Array<Interface_TokenComponent>(totalCoins);
-
-  const tokenDecimals = BigInt.fromString("10").pow(token.decimals).toString();
-  const totalSupply: Big = Big.of(token.totalSupply.toString()) / Big.of(tokenDecimals);
-
-  let unresolvedComponents: i32 = 0;
-
-  for (let i = 0; i < totalCoins; i++) {
-    const underlyingTokenAddress: string = coins[i];
-    const underlyingToken: Interface_Token = changetype<Interface_Token>(
-      Token_Query.getToken({
-        address: underlyingTokenAddress,
-        m_type: Token_TokenType.ERC20,
-      }).unwrap(),
-    );
-    if (!underlyingToken) {
-      unresolvedComponents++;
-      continue;
-    }
-    const underlyIngDecimals = BigInt.fromString("10").pow(underlyingToken.decimals).toString();
-    const balance: Big = Big.of(balances[i]) / Big.of(underlyIngDecimals);
-    const rate = (balance / totalSupply).toString();
-    components[i] = {
-      tokenAddress: underlyingTokenAddress,
-      unresolvedComponents: 0,
+  });
+  if (underlyingTokenAddressRes.isErr) {
+    return {
+      tokenAddress: token.address,
+      unresolvedComponents: 1,
       components: [],
-      rate: rate,
+      rate: "1",
     };
   }
 
+  // calculate rate
+  const shareResult = Ethereum_Query.callContractView({
+    address: token.address,
+    method: "function pricePerShare() external view returns (uint256)",
+    args: [],
+    connection: connection,
+  });
+  if (shareResult.isErr) {
+    throw new Error("Invalid Yearn protocol token: " + token.address);
+  }
+  const decimals = BigInt.fromUInt16(10).pow(token.decimals).toString();
+  const rate = (Big.of(shareResult) / Big.of(decimals)).toString();
+
   return {
     tokenAddress: token.address,
-    unresolvedComponents: unresolvedComponents,
-    components: components,
+    unresolvedComponents: 0,
+    components: [
+      {
+        tokenAddress: underlyingTokenAddressRes.unwrap(),
+        unresolvedComponents: 0,
+        components: [],
+        rate,
+      },
+    ],
     rate: "1",
   };
 }
--- packages/core-sdk/resolvers/token-resolvers/resolvers/yearn/src/query/functions/isValidProtocolToken.ts
+++ packages/core-sdk/resolvers/token-resolvers/resolvers/yearn/src/query/functions/isValidProtocolToken.ts
@@ -1,3 +1,4 @@
+import { YEARN_REGISTERY_ADAPTER_V2, YEARN_REGISTRY_ADAPTER_IRON_BANK } from "../constants";
 import {
   env,
   Ethereum_Connection,
@@ -6,12 +7,57 @@ import {
   QueryEnv,
 } from "../w3";
 
-function isValidYearnVaultV2(lpTokenAddress: string, connection: Ethereum_Connection): boolean {
-
+function isRegisteredYearn(
+  yTokenAddress: string,
+  registryAddress: string,
+  connection: Ethereum_Connection,
+): boolean {
+  const isRegisteredRes = Ethereum_Query.callContractView({
+    address: registryAddress,
+    method:
+      "function assetsStatic(address[] memory _assetsAddresses) public view returns (AssetStatic[] memory)",
+    args: [`["${yTokenAddress}"]`],
+    connection: connection,
+  });
+  if (isRegisteredRes.isErr) {
+    return false;
+  }
+  const assetImmutables: string[] = isRegisteredRes.unwrap().split(",");
+  return assetImmutables[0] == yTokenAddress;
 }
 
-function isValidYearnVaultV1(lpTokenAddress: string, connection: Ethereum_Connection): boolean {
+function isActiveYearn(yTokenAddress: string, connection: Ethereum_Connection): boolean {
+  const isActiveRes = Ethereum_Query.callContractView({
+    address: yTokenAddress,
+    method: "function isActive() external view returns (bool)",
+    args: [],
+    connection: connection,
+  });
+  if (isActiveRes.isErr) {
+    return false;
+  }
+  return isActiveRes.unwrap() == "true";
+}
 
+function isValidYearnVaultV2(yTokenAddress: string, connection: Ethereum_Connection): boolean {
+  return (
+    isRegisteredYearn(yTokenAddress, YEARN_REGISTERY_ADAPTER_V2, connection) &&
+    isActiveYearn(yTokenAddress, connection)
+  );
+}
+
+function isValidYearnIronBank(yTokenAddress: string, connection: Ethereum_Connection): boolean {
+  return (
+    isRegisteredYearn(yTokenAddress, YEARN_REGISTRY_ADAPTER_IRON_BANK, connection) &&
+    isActiveYearn(yTokenAddress, connection)
+  );
+}
+
+function isValidYearnVaultV1(yTokenAddress: string, connection: Ethereum_Connection): boolean {
+  return (
+    isRegisteredYearn(yTokenAddress, YEARN_REGISTERY_ADAPTER_V2, connection) &&
+    isActiveYearn(yTokenAddress, connection)
+  );
 }
 
 export function isValidProtocolToken(input: Input_isValidProtocolToken): boolean {
@@ -20,6 +66,8 @@ export function isValidProtocolToken(input: Input_isValidProtocolToken): boolean
 
   if (input.protocolId == "yearn_vault_v2") {
     return isValidYearnVaultV2(input.tokenAddress, connection);
+  } else if (input.protocolId == "yearn_iron_bank") {
+    return isValidYearnIronBank(input.tokenAddress, connection);
   } else if (input.protocolId == "yearn_vault_v1") {
     return isValidYearnVaultV1(input.tokenAddress, connection);
   } else {
