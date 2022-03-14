a9bb054c2a415410a51da28eedf5f6d6a17d2a4d
krisbitney <kristoferbitney@hotmail.com>
2022-03-13 19:13:50 -0500

    curve token resolver - refactored tests

--- packages/core-sdk/resolvers/token-resolvers/resolvers/curve/src/query/__tests__/e2e/integration.spec.ts
+++ packages/core-sdk/resolvers/token-resolvers/resolvers/curve/src/query/__tests__/e2e/integration.spec.ts
@@ -1,13 +1,16 @@
-import { QueryApiResult, Web3ApiClient } from "@web3api/client-js";
+import { Web3ApiClient } from "@web3api/client-js";
 import { buildAndDeployApi, initTestEnvironment, stopTestEnvironment } from "@web3api/test-env-js";
 import path from "path";
 
+import { Interface_TokenComponent as TokenComponent } from "../../w3";
 import { getPlugins } from "../utils";
-import { GetTokenComponentsResponse, IsValidProtocolTokenResponse, TokenComponent } from "./types";
+import { getTokenComponents, isValidProtocolToken } from "./apiCalls";
 
 jest.setTimeout(300000);
 
 describe("Ethereum", () => {
+  let curveEnsUri: string;
+  let tokenEnsUri: string;
   let client: Web3ApiClient;
   let testEnvState: {
     ethereum: string;
@@ -31,8 +34,6 @@ describe("Ethereum", () => {
   });
 
   describe("curve", () => {
-    let curveEnsUri: string;
-    let tokenEnsUri: string;
     beforeAll(async () => {
       // deploy api
       const curveApiPath: string = path.join(path.resolve(__dirname), "..", "..", "..", "..");
@@ -52,120 +53,48 @@ describe("Ethereum", () => {
       );
       tokenEnsUri = `ens/testnet/${tokenApi.ensDomain}`;
     });
+
     describe("isValidTokenProtocol", () => {
-      const isValidProtocolToken = async (
-        tokenAddress: string,
-        protocolId: string,
-      ): Promise<QueryApiResult<IsValidProtocolTokenResponse>> => {
-        const response = await client.query<IsValidProtocolTokenResponse>({
-          uri: curveEnsUri,
-          query: `
-            query IsValidProtocolToken($tokenAddress: String, $protocolId: String) {
-              isValidProtocolToken(
-                tokenAddress: $tokenAddress,
-                protocolId: $protocolId
-              )
-            }
-          `,
-          variables: {
-            tokenAddress: tokenAddress,
-            protocolId: protocolId,
-          },
-          config: {
-            envs: [
-              {
-                uri: curveEnsUri,
-                query: {
-                  connection: {
-                    networkNameOrChainId: "MAINNET",
-                  },
-                },
-              },
-            ],
-          },
-        });
-        return response;
-      };
       test("curve 3pool gauge", async () => {
         const result = await isValidProtocolToken(
           "0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A",
           "curve_fi_gauge_v2",
+          curveEnsUri,
+          client,
         );
 
-        expect(result.errors).toBeFalsy();
+        expect(result.error).toBeFalsy();
         expect(result.data).toBeTruthy();
-        expect(result.data?.isValidProtocolToken).toBe(true);
+        expect(result.data).toBe(true);
       });
 
       test("curve bBTC metapool", async () => {
         const result = await isValidProtocolToken(
           "0x071c661B4DeefB59E2a3DdB20Db036821eeE8F4b",
           "curve_fi_pool_v2",
+          curveEnsUri,
+          client,
         );
 
-        expect(result.errors).toBeFalsy();
+        expect(result.error).toBeFalsy();
         expect(result.data).toBeTruthy();
-        expect(result.data?.isValidProtocolToken).toBe(true);
+        expect(result.data).toBe(true);
       });
     });
 
     describe("getTokenComponents", () => {
-      const getTokenComponents = async (
-        tokenAddress: string,
-        protocolId: string,
-      ): Promise<QueryApiResult<GetTokenComponentsResponse>> => {
-        const response = await client.query<GetTokenComponentsResponse>({
-          uri: curveEnsUri,
-          query: `
-            query GetTokenComponents($tokenAddress: String!) {
-              getTokenComponents(
-                tokenAddress: $tokenAddress,
-                protocolId: $protocolId
-              )
-            }
-          `,
-          variables: {
-            tokenAddress: tokenAddress,
-            protocolId: protocolId,
-          },
-          config: {
-            redirects: [
-              {
-                from: "ens/token.defiwrapper.eth",
-                to: tokenEnsUri,
-              },
-            ],
-            envs: [
-              {
-                uri: curveEnsUri,
-                query: {
-                  connection: {
-                    networkNameOrChainId: "MAINNET",
-                  },
-                },
-              },
-              {
-                uri: "ens/token.defiwrapper.eth",
-                query: {
-                  connection: {
-                    networkNameOrChainId: "MAINNET",
-                  },
-                },
-              },
-            ],
-          },
-        });
-        return response;
-      };
       test("curve 3pool", async () => {
         const result = await getTokenComponents(
           "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490",
           "curve_fi_pool_v2",
+          tokenEnsUri,
+          curveEnsUri,
+          client,
         );
 
-        expect(result.errors).toBeFalsy();
+        expect(result.error).toBeFalsy();
         expect(result.data).toBeTruthy();
-        expect(result.data?.getTokenComponents).toMatchObject({
+        expect(result.data).toMatchObject({
           rate: "1",
           unresolvedComponents: 0,
           tokenAddress: "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490",
@@ -187,7 +116,7 @@ describe("Ethereum", () => {
             },
           ],
         });
-        const tokenComponent = result.data?.getTokenComponents as TokenComponent;
+        const tokenComponent = result.data as TokenComponent;
         let sum = 0;
         tokenComponent.components.forEach((x: TokenComponent) => {
           sum += +x.rate;
