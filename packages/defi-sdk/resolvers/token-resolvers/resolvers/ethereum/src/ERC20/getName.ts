7fd5d4ef827647f9ed76c58a16171b39bf722664
Niraj Kamdar <nirajkamdar27@pm.me>
2022-08-22 18:37:29 +0400

    fix(token-resolver): issues

--- packages/defi-sdk/resolvers/token-resolvers/resolvers/ethereum/src/ERC20/getName.ts
+++ packages/defi-sdk/resolvers/token-resolvers/resolvers/ethereum/src/ERC20/getName.ts
@@ -1,7 +1,7 @@
 import { BigInt } from "@polywrap/wasm-as";
 
 import { hexToUtfStr } from "../utils";
-import { Ethereum_Connection, Ethereum_Module } from "../wrap";
+import { Ethereum_Module } from "../wrap";
 
 export function getName(address: string): string | null {
   if (address.toLowerCase() == "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
@@ -11,15 +11,16 @@ export function getName(address: string): string | null {
   const nameResult = Ethereum_Module.callContractView({
     address: address,
     method: "function name() external view returns (string memory)",
-    args: []
+    args: [],
+    connection: null,
   });
   if (nameResult.isOk) return nameResult.unwrap();
 
-  // FIXME: This won't work since first call is throwing an error in case of symbol isn't string
   const bytesNameResult = Ethereum_Module.callContractView({
     address: address,
     method: "function name() external view returns (bytes32)",
-    args: []
+    args: [],
+    connection: null,
   });
   if (bytesNameResult.isOk) return hexToUtfStr(bytesNameResult.unwrap());
 
